5b2ed6c0080b8d9042d19e146c7f06fec114035d
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2023-06-25 17:26:12 -0500

    ci

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import './App.css';
 import CreateUser from './screens/adminScreen/CreateUser';
 import ControlPanelAdmin from './screens/adminScreen/ControlPanelAdmin';
 import IconTextInput from './componente/IconTextInput';
+import ButtonIcon from './componente/ButtonIcon'
 
 function App() {
-  let juan = 1;
-  switch (juan === 2) {
+  let juan = 3;
+  switch (juan) {
     case 1:
+      return (
       <CreateUser />
-      break;
+      );
     case 2:
+      return (
       <ControlPanelAdmin />
-      break;
+      );
     case 3:
-      <IconTextInput />
-      break;
+      return (<div>
+        <ButtonIcon text = "panita"  w="30vw" h="5vh" img="person-add-outline"/>
+      </div>
+      
+
+      )
   }
 }  
 
--- src/componente/ButtonIcon.jsx
+++ src/componente/ButtonIcon.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import "./css/ButtonIcon.css";
 
+function ButtonIcon(props) {
+  const { w, h, text, img} = props;
 
-export default function Button(props) {
-    const { w, h, text, img} = props;
-    return (
-        <div className="containerRow1">
-        <button style={{ width: w, height: h }} className="buttonStyle1">
-          <div className="buttonContent">
-            <img src={img} className="imgStyle" alt="icon" />
-            <div className="buttonText">{text}</div>
-          </div>
-        </button>
+  return (
+    <button className="ButtonInputWrapper">
+      <ion-icon name={img}></ion-icon>
+      <div className="textContainer">
+        <p className="textContainer1">{text}</p>
       </div>
-    )
+    </button>
+  );
 }
+
+export default ButtonIcon;
