59e1eb4c4f469b9e50adfe6a4cf324480b78f4e4
Santiagokmids <sntgtrochez@gmail.com>
2023-07-02 18:19:39 -0500

    feat: evaluate rule working

--- src/componente/PopUpEvaluateRule.jsx
+++ src/componente/PopUpEvaluateRule.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Header from './Header'
-import ButtonType1 from './ButtonType1'
 import './css/PopUpDropdown.css'
 import Button from './Button';
 import Dropdown from './Dropdown';
-import FormInput from './FormInput';
 
 export default function PopUpDropdown(props) {
-  const { button2, button1, textHeader, selectOptions, onClickHeader, valueToSet, onButton, value, idValue, idValueToSet} = props;
+  const { button2, textHeader, selectRules, selectRecords, onClickHeader, valueToSet, onButton, value, idValue, idValueToSet} = props;
 
   return (
     <div className="popup">
@@ -15,8 +13,8 @@ export default function PopUpDropdown(props) {
         <div style={{ width: "50vw", height: "50vh" }} className="Container">
           <Header buttonText="Regresar" headerText={textHeader} w="100%" onClick={onClickHeader} />
           <div className='ButtonContainer'>
-            <FormInput type="text" placeholder={button1} h="8vh" w="40vw" fontsize="2.7rem" value={idValue} onChange={newIdValue => idValueToSet(newIdValue)} />
-            <Dropdown selectOptions={selectOptions} t="5%" optionD={button2} w="40vw" h="8vh" fontsize="2.5rem" value={value} onChange={newValue => valueToSet(newValue)}/>
+            <Dropdown selectOptions={selectRecords} t="5%" optionD="Seleccionar id del registro" w="40vw" h="8vh" fontsize="2.5rem" value={idValue} onChange={newValue => idValueToSet(newValue)}/>
+            <Dropdown selectOptions={selectRules} t="5%" optionD={button2} w="40vw" h="8vh" fontsize="2.5rem" value={value} onChange={newValue => valueToSet(newValue)}/>
             <Button marginT="5%" text="Confirmar" w="80%" h="20%" fontsize="2.7rem" onClick={onButton} />
           </div>
         </div>
--- src/screens/recordManagementScreen/ControlePanelRecordManagement.jsx
+++ src/screens/recordManagementScreen/ControlePanelRecordManagement.jsx
@@ -1,50 +1,168 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from "../../componente/Header";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import "./css/ControlPanelRecord.css";
 import ButtonIcon from "../../componente/ButtonIcon";
 import Button from "../../componente/Button";
 import PopUpDropdown from "../../componente/PopUpDropdown";
+import PopUpEvaluateRule from "../../componente/PopUpEvaluateRule";
 
-export default function ControlePanelRecordManagement() {
-  const [isPopupOpen, setPopupOpen] = useState(false);
+const baseUrl = "http://localhost:8091"
+
+function ControlePanelRecordManagement() {
+
+  const [rules, setRules] = useState([]);
+  const [records, setRecords] = useState([]);
+
+  const [optionRecords, setOptionRecords] = useState([]);
+  const [optionRules, setOptionRules] = useState([]);
+  const [isPopupOpen, setPopupOpen] = useState("");
+  const [record, setRecord] = useState("");
+  const [ruleSelected, setRuleSelected] = useState("");
+
   const navigation = useNavigate();
+
+  useEffect(() => {
+
+    async function getData() {
+
+      const resultRules = await getRules();
+      setRules(resultRules);
+
+      const resultRecords = await getRecords();
+      setRecords(resultRecords);
+    }
+
+    getData();
+    
+    const allRules = rules.map(rule => ({
+      label: rule.ruleName,
+      value: rule.ruleName
+    }));
+
+    const allRecords = records.map(record => ({
+      label: record.record_id,
+      value: record.record_id
+    }));
+
+    setOptionRecords(allRecords);
+    setOptionRules(allRules);
+
+  }, [rules, records]);
+
   const handleLogout = async (event) => {
     event.preventDefault();
     navigation("/login");
   };
-  const handleClick = () => {
-    console.log("Botón clickeado");
-    setPopupOpen(true);
-  };
-  const createRecord = () => {
-    console.log("Botón clickeado");
+
+  const handleCreateRecord = async (event) => {
+    event.preventDefault();
     navigation("/createRecord");
+  };
+
+  const handleOpenRecordManagement = async (event) => {
+    event.preventDefault();
+    setPopupOpen(0);
+  };
+
+  const handleOpenEvaluateRule = async (event) => {
+    event.preventDefault();
+    setPopupOpen(1);
   };
 
   const handleClose = () => {
     setPopupOpen(false);
   };
 
+  const handleEvaluateRule = async (event) => {
+    event.preventDefault();
+    
+    try{
+      const response = await axios.get(baseUrl + "/rules/evaluate/"+record+"/"+ruleSelected,
+        {
+          headers:{
+            "Access-Control-Allow-Origin": baseUrl,
+            "MediaType" : "application/json",
+          }
+        }
+      );
+
+      if (response.status === 200) {
+        alert("El resultado de evaluar el registro con id "+record+" con la regla "+ruleSelected+" es: "+eval(response.data));
+        handleClose();
+      }
+    }
+    catch (error) {
+      console.log(error)
+      alert("No se pudo evaluar la regla. "+error.response.data.details[0].errorMessage)
+    } 
+  };
+
   const selectOptions = [
     { label: "Eliminar registro", value: "Eliminar registro" },
-    ];
+  ];
+
   return (
     <div >
     <Header buttonText="Cerrar sesion" headerText="Panel de control" onClick={handleLogout}/>
     <body className="body">
       <div className="containerBase" style={{width:"80vw"}}>
           <div className="containerBase2">
-            <ButtonIcon onClick={handleClick} marginL="1vw" marginT="1vw" text = "Crear registro"  w="24vw" h="6vh" img="reader-outline" onClick={createRecord}/>
-            <ButtonIcon onClick={handleClick} marginL="1vw" marginT="1vw" text = "Gestionar registro"  w="24vw" h="6vh" img="settings-outline"/>
-            <Button text = "Evaluar registro"  w="24vw" h="6vh" marginL="1vw" marginT="1vw"/>
+            <ButtonIcon onClick={handleCreateRecord} marginL="1vw" marginT="1vw" text = "Crear registro"  w="24vw" h="6vh" img="reader-outline"/>
+            <ButtonIcon onClick={handleOpenRecordManagement} marginL="1vw" marginT="1vw" text = "Gestionar registro"  w="24vw" h="6vh" img="settings-outline"/>
+            <Button text = "Evaluar registro"  w="24vw" h="6vh" marginL="1vw" marginT="1vw" onClick={handleOpenEvaluateRule}/>
           </div>
       </div>
-      {isPopupOpen && (
+      {isPopupOpen === 0 && (
             <PopUpDropdown button2="Seleccionar accion" button1="Ingrese el ID" textHeader="Gestionar usuarios" selectOptions={selectOptions} onClickHeader={handleClose} />
+      )}
+
+      {isPopupOpen === 1 && (
+            <PopUpEvaluateRule button2="Seleccionar regla" button1="Ingrese el ID del registro" textHeader="Evaluar regla" selectRules={optionRules}
+              selectRecords={optionRecords}
+              onClickHeader={handleClose}
+              idValue={record}
+              idValueToSet={setRecord}
+              value={ruleSelected}
+              valueToSet={setRuleSelected}
+              onButton={handleEvaluateRule}
+            />
       )}
     </body>
   </div>
   )
 }
+
+async function getRules(){
+
+  const rules = await axios.get(
+    baseUrl+"/rules",
+    {
+      headers:{
+        "Access-Control-Allow-Origin": baseUrl,
+        "MediaType" : "application/json",
+      }
+    }
+  )
+
+  return rules.data;
+}
+
+async function getRecords(){
+
+  const records = await axios.get(
+    baseUrl+"/table/records",
+    {
+      headers:{
+        "Access-Control-Allow-Origin": baseUrl,
+        "MediaType" : "application/json",
+      }
+    }
+  )
+
+  return records.data.records;
+}
+
+export default ControlePanelRecordManagement;
