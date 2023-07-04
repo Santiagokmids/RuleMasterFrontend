1bfb767d94214b759c549d33937ce55d1502414b
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2023-07-04 17:39:02 -0500

    ci

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
-import { Auth0Provider } from '@auth0/auth0-react';
 
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
@@ -14,9 +13,7 @@ const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
       <App />
-    </Auth0Provider>  
   </React.StrictMode>
 );
 
